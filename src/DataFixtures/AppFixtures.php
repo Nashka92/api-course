db30a7c7b60045b347f2d907ae430954e5db6e8a
Anna RAACH <annagraph92@gmail.com>
2022-05-23 16:11:12 +0200

    Securité du mot de passe, mise en place de la bdd

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,39 +3,59 @@
 namespace App\DataFixtures;
 
 use Faker\Factory;
-use App\Entity\Customer;
-use App\Entity\Invoice;
 use App\Entity\User;
+use App\Entity\Invoice;
+use App\Entity\Customer;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface; 
 
 class AppFixtures extends Fixture
 {
+
+    /**
+     * l'encodeur de mots de passe, ce qui permet de hasher le mdp dans la BDD
+     *
+     * @var UserPasswordHasherInterface
+     */
+    private $userPasswordHasherInterface;
+
+    //C'est avec le construct que je vais devoir mettre en place le hashage des mdp des users
+    public function __construct(UserPasswordHasherInterface $userPasswordHasherInterface)
+    {
+      $this->userPasswordHasherInterface = $userPasswordHasherInterface;  
+
+    }
+  
     public function load(ObjectManager $manager): void
     {
+        
         $faker = Factory::create('fr_FR');
-
-        //chrono = n° de facture
-        $chrono = 1;
 
 
         //créer de faux user
         for ($u = 0; $u < 10; $u++) {
             $user = new User();
+            //chrono = n° de facture, elle doit repartir à 1 a chaque nouveau client car un client peut avoir 1 ou plusieurs factures
+            $chrono = 1;
             $user->setFirstname($faker->firstName())
                 ->setLastName($faker->lastName)
                 ->setEmail($faker->email)
-                ->setPassword("password");
+                //setPassword("password");
+                //on initialise et on appelle la fonction hashPassword 
+                ->setPassword($this->userPasswordHasherInterface->hashPassword($user, "password"));
 
             $manager->persist($user);
-
+ 
             //créer de faux profil client
             for ($c = 0; $c < 30; $c++) {
                 $customer = new Customer();
                 $customer->setFirstName($faker->firstName())
                     ->setLastName($faker->lastName)
                     ->setCompany($faker->company)
-                    ->setEmail($faker->email);
+                    ->setEmail($faker->email)
+                    //le customer doit être relié à l'user 
+                    ->setUser($user);
 
                 $manager->persist($customer);
 
